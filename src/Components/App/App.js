fa494d5157a1827c0da9e447003ecb1b52a119a8
Kevin Loughead <kvnloughead@gmail.com>
2021-03-04 12:47:25 -0500

    feat: Section markup and styles desktop res

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,24 +1,12 @@
-import logo from '../../logo.svg';
 import './App.css';
+import Section from '../Section/Section'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Section type='products' layout='columns' title='Wide choice of transport robots'></Section>
+      <Section type='benefits' layout='rows' title='Benefits'></Section>
+    </>
   );
 }
 
