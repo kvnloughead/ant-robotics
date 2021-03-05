7d94aea06687a928d44b3b32e737d770218a5f5e
Kevin Loughead <kvnloughead@gmail.com>
2021-03-04 20:10:47 -0500

    feat: Section component responsiveness

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Section from '../Section/Section'
+
+function App() {
+  return (
+    <>
+      <Section type='products' layout='two-columns' title='Wide choice of transport robots'></Section>
+      <Section type='benefits' layout='four-columns' title='Benefits'></Section>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Section/Section.js
@@ -0,0 +1,24 @@
+import './Section.css';
+
+function Section(props) {
+  return (
+    <section className={`section section_type_${props.type} section_layout_${props.layout}`}>
+      <h2 className="section__title">{props.title}</h2>
+      {/* delete placeholders */}
+      {props.layout === 'two-columns' ? (
+        <div className='placeholder_two-columns'></div>
+      ) : (
+        <div className='placeholder_four-columns'>
+          <div className='placeholder__card'></div>
+          <div className='placeholder__card'></div>
+          <div className='placeholder__card'></div>
+          <div className='placeholder__card'></div>
+        </div>
+      )
+      }
+      {/* delete placeholders */}
+    </section>
+  );
+}
+
+export default Section;
