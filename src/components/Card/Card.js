79e64fe722fa0cd0bb7e53636b1deb8478b9bbef
Kevin Loughead <kvnloughead@gmail.com>
2021-03-06 23:30:54 -0500

    feat: benefits configurability

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,8 +4,8 @@ function Card(props) {
   return (
     <li className='card'>
       <div className={`card__image card__image_type_${props.type}`} style={{backgroundImage: `url(${props.card.image})`}}></div>
-      <h3 className='card__title'>{props.card.title}</h3>
-      <p className='card__text'>{props.card.text}</p>
+      <h3 className={`card__title card__title_type_${props.type}`}>{props.card.title}</h3>
+      <p className={`card__text card__text_type_${props.type}`}>{props.card.text}</p>
     </li>
   );
 }
