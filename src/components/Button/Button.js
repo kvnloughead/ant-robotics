fda84d191802e3f9e82adef1b2cb7a3531eb6595
HCQpersonal <personalz.hcq@gmail.com>
2021-03-07 17:24:06 -0800

    feat: add ability to change color on the Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,7 @@ import './Button.css';
 
 function Button(props) {
   return (
-    <button className='button' onClick={props.onClick}>
+    <button className='button' onClick={props.onClick} style={props.style}>
       {props.label}
     </button>
   );
