c63e3c52f15d9e330801130df83bf91e69230bd1
hotcheetoqueen <jessicalperelman@gmail.com>
2021-03-07 19:06:25 -0800

    feat: map products in carousel list from config

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -4,38 +4,26 @@ import Button from '../Button/Button';
 function Carousel(props) {
     return(
         <>
-            <div className='carousel'>
+            <ul className='carousel'>
                 <image className='carousel-arrow carousel-arrow_left'></image>
-                <div className='carousel-item'>
-                    <image className='carousel-item__image' src={products.image}></image>
-                    <div className='carousel-item__details'>
-                        <h2 className='carousel-item__title'>OTTO 100 (pending) {props.title}</h2>
-                        <ul className='carousel-item__specs'>
-                            <li className='carousel-item__spec'>
-                                <span className='carousel-item__spec-type'>Spec type: </span>Spec value
-                            </li>
-                            <li className='carousel-item__spec'>
-                                <span className='carousel-item__spec-type'>Spec type: </span>Spec value
-                            </li>
-                            <li className='carousel-item__spec'>
-                                <span className='carousel-item__spec-type'>Spec type: </span>Spec value
-                            </li>
-                            <li className='carousel-item__spec'>
-                                <span className='carousel-item__spec-type'>Spec type: </span>Spec value
-                            </li>
-                            <li className='carousel-item__spec'>
-                                <span className='carousel-item__spec-type'>Spec type: </span>Spec value
-                            </li>
-                            {/* for (const [key, value] of Object.entries(products).map(([key,value], i) =>
-                            <li className='carousel-item__spec' card={[key, value]} key={props.cards.indexOf(card)} value={props.cards.indexOf(card)}>
-                                <span className='carousel-item__spec-type'>{props.data.key}</span>{props.data.value}
-                            </li>)})  { */}
-                        </ul>
-                        <Button className='carousel-item__button' label={'Get more info'} style={{ background: 'black' }} />
-                    </div>
-                </div>
+                {props.products.map((product) => (
+                    <li className='carousel-item'>
+                        <image className='carousel-item__image' src={product.image}></image>
+                        <div className='carousel-item__details'>
+                            <h3 className='carousel-item__title'>{product.title}</h3>
+                            <ul className='carousel-item__specs'>
+                            {Object.entries(product.data).map(([key,value]) => (
+                                <li className='carousel-item__spec'>
+                                    <span className='carousel-item__spec-type'>{key}</span>{value}
+                                </li>
+                            ))}
+                            </ul>
+                            <Button className='carousel-item__button' label={'Get more info'} style={{ background: 'black' }} />
+                        </div>
+                    </li>
+                ))}
                 <image className='carousel-arrow carousel-arrow_right'></image>
-            </div>
+            </ul>
         </>
     );
 }
--- src/utils/config.js
+++ src/utils/config.js
@@ -23,9 +23,9 @@ export const benefits = [
 
 export const products = [
   {
-    image: "./public/images/products-1-OTTO.svg",
-    title: "OTTO 100 (pending)",
-    data: {
+    "image": "./public/images/products-1-OTTO.svg",
+    "title": "OTTO 100 (pending)",
+    "data": {
       "Max payload: ": "220 lbs (100kg)",
       "Size: ": "740 x 550 x 310 mm",
       "Max speed: ": "4.3 mph",
@@ -33,15 +33,15 @@ export const products = [
       "Charging time: ": "1.2hrs (0-80%: 0.75 hrs)"
     },
   },
-  {
-    image: "./public/images/products-2-placeholder.svg",
-    title: "Placeholder",
-    data: {
-      "Max payload: ": "220 lbs (100kg)",
-      "Size: ": "740 x 550 x 310 mm",
-      "Max speed: ": "4.3 mph",
-      "Running time: ": "4 hrs or 9 mi",
-      "Charging time: ": "1.2hrs (0-80%: 0.75 hrs)"
-    },
-  }
+  // {
+  //   "image": "./public/images/products-2-placeholder.svg",
+  //   "title": "Placeholder",
+  //   "data": {
+  //     "Max payload: ": "220 lbs (100kg)",
+  //     "Size: ": "740 x 550 x 310 mm",
+  //     "Max speed: ": "4.3 mph",
+  //     "Running time: ": "4 hrs or 9 mi",
+  //     "Charging time: ": "1.2hrs (0-80%: 0.75 hrs)"
+  //   },
+  // }
 ];
