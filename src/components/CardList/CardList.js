5feb75f67bb860a25e2cdc342ad48a0655ce994d
Kevin Loughead <kvnloughead@gmail.com>
2021-03-16 20:25:12 -0400

    fix: fix issues per review

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,11 +1,11 @@
 import './CardList.css';
 import Card from '../Card/Card';
 
-function CardList(props) {
+function CardList({ cards, type }) {
   return (
-    <ul className={`card-list card-list_type_${props.type}`}>
-      {props.cards.map((card) => (
-        <Card card={card} type={props.type} key={props.cards.indexOf(card)} />
+    <ul className={`card-list card-list_type_${type}`}>
+      {cards.map((card) => (
+        <Card card={card} type={type} key={cards.indexOf(card)} />
       ))}
     </ul>
   );
