4d55d20a378af5ca51a55f9d86ebb14e099da0be
Kevin Loughead <kvnloughead@gmail.com>
2021-03-20 11:21:30 -0400

    fix: remove all calc declarations

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,12 +3,12 @@ import './Form.css';
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
 
-function Form(props) {
+function Form({ modalType, closeModal }) {
   return (
     <>
-      <form className="form" id={`${props.modalType}-form`} name={`${props.modalType}Form`} action="#" noValidate>
+      <form className="form" id={`${modalType}-form`} name={`${modalType}Form`} action="#" noValidate>
         <h2 className="form__title">Write us</h2>
-        <button className="form__close-button" type="button" aria-label="close-modal" onClick={props.closeModal} />
+        <button className="form__close-button" type="button" aria-label="close-modal" onClick={closeModal} />
         <TextInput type="name" title="Name" />
         <TextInput type="email" title="Email*" required />
         <TextInput type="textarea" title="Message" />
@@ -19,8 +19,8 @@ function Form(props) {
         aria-label="close-modal"
         tabIndex={0}
         className="overlay"
-        onClick={props.closeModal}
-        onKeyDown={props.closeModal}
+        onClick={closeModal}
+        onKeyDown={closeModal}
       />
     </>
   );
