91993886de9cfaf375763d2b381519196c4684bf
Kevin Loughead <kvnloughead@gmail.com>
2021-03-30 11:43:04 -0400

    fix: display of cards when res > 1281 or with non-standard numbers of cards

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -3,7 +3,7 @@ import Card from '../Card/Card';
 
 function CardList({ cards, type }) {
   return (
-    <ul className={`card-list card-list_type_${type}`}>
+    <ul className={`card-list card-list_type_${type} ${cards.length % 4 === 0 && 'card-list_spacing_space-between'}`}>
       {cards.map((card) => (
         <Card card={card} type={type} key={cards.indexOf(card)} />
       ))}
