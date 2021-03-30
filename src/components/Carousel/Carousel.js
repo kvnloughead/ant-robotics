a750ac9e511b8e690d5a1385ca38022cc161ac03
hotcheetoqueen <jessicalperelman@gmail.com>
2021-03-29 22:32:34 -0700

    feat: set up initial config and mapping logic for carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,35 +1,38 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import './Carousel.css';
 import Button from '../Button/Button';
-// import { products } from '../../config/products';
+import { products, arrows } from '../../config/products';
 
 export default function Carousel(props) {
-  // const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  // const handleArrowClick = (evt) => {
-  //   const isLeftArrow = evt.currentTarget.classList.contains('carousel-arrow_left');
-  //   const newIndex = currentIndex + (isLeftArrow ? -1 : 1);
-  //   if (newIndex <= products.length - 1) {
-  //     setCurrentIndex(newIndex < 0 ? 0 : newIndex);
-  //   }
-  // };
+  const handleArrowClick = (evt) => {
+    const isLeftArrow = evt.currentTarget.classList.contains('carousel-arrow_left');
+    const newIndex = currentIndex + (isLeftArrow ? -1 : 1);
+    if (newIndex <= products.length - 1) {
+      setCurrentIndex(newIndex < 0 ? 0 : newIndex);
+    }
+  };
 
   return (
     <>
       <ul className="carousel">
-        <img className="carousel-arrow carousel-arrow_left" src="../../images/products/product_carousel-arrow_left.svg" alt="left slider arrow" />
-        {/* onClick={handleArrowClick} */}
+        <button className="carousel-arrow carousel-arrow_left" type="button" alt="left slider arrow" 
+          onClick={handleArrowClick}
+          style={{ backgroundImage: `url(${currentIndex > 0 ? arrows.left : arrows.leftInactive})` }}
+        />
         {props.products.map((product) => (
           <li className="carousel-item">
-            <img className="carousel-item__image" src="../../images/products/product_1-otto.svg" alt="robot product discussed in each card" />
-            {/* src={product.image} with map OR src={products[currentIndex].image with state} */}
+            <img className="carousel-item__image" alt="robot product discussed in each card"
+              src={products[currentIndex].image}
+            />
             <div className="carousel-item__details">
-              <h3 className="carousel-item__title">{product.title}</h3>
+              <h3 className="carousel-item__title">{products[currentIndex].title}</h3>
               <ul className="carousel-item__specs">
-                {Object.entries(product.data).map(([key, value]) => (
+                {Object.entries(product.data).map(([category, value]) => (
                   <li className="carousel-item__spec">
-                    <span className="carousel-item__spec-type">{key}</span>
-                    {value}
+                    <span className="carousel-item__spec-type">{products[currentIndex].data[category]}</span>
+                    {products[currentIndex].data[value]}
                   </li>
                 ))}
               </ul>
@@ -37,7 +40,10 @@ export default function Carousel(props) {
             </div>
           </li>
         ))}
-        <img className="carousel-arrow carousel-arrow_right" src="../../images/products/product_carousel-arrow_left.svg" alt="right slider arrow" />
+        <button className="carousel-arrow carousel-arrow_right" type="button" alt="right slider arrow"
+          onClick={handleArrowClick}
+          style={{ backgroundImage: `url(${currentIndex < products.length - 1 ? arrows.right : arrows.rightInactive})` }}
+        />
       </ul>
     </>
   );
--- src/config/products.js
+++ src/config/products.js
@@ -10,15 +10,22 @@ export const products = [
       'Charging time: ': '1.2hrs (0-80%: 0.75 hrs)',
     },
   },
-  // {
-  //   "image": "./public/images/products-2-placeholder.svg",
-  //   "title": "Placeholder",
-  //   "data": {
-  //     "Max payload: ": "220 lbs (100kg)",
-  //     "Size: ": "740 x 550 x 310 mm",
-  //     "Max speed: ": "4.3 mph",
-  //     "Running time: ": "4 hrs or 9 mi",
-  //     "Charging time: ": "1.2hrs (0-80%: 0.75 hrs)"
-  //   },
-  // }
+  {
+    image: 'https://images.unsplash.com/photo-1546776310-eef45dd6d63c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1339&q=80',
+    title: 'Placeholder',
+    data: {
+      'Max payload: ': 'Placeholder',
+      'Size: ': 'Placeholder',
+      'Max speed: ': 'Placeholder',
+      'Running time: ': 'Placeholder',
+      'Charging time: ': 'Placeholder'
+    },
+  }
 ];
+
+export const arrows = {
+  left: '../../images/products/product_carousel-arrow_left.svg',
+  right: '../../images/products/product_carousel-arrow_left.svg',
+  leftInactive: './images/gallery/left-arrow-inactive.png',
+  rightInactive: './images/gallery/left-arrow-inactive.png',
+};
