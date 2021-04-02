7ed5f363446ad224ce3e97c429099019ec9ef647
Kevin Loughead <kvnloughead@gmail.com>
2021-04-01 20:21:18 -0400

    fix: make various fixes prior to merging - fix eslint warnings - fix console error (no unique key) - remove package-lock - fix checkmark not displaying - make robot-facts summary appear

--- src/components/SectionContext/SectionContext.js
+++ src/components/SectionContext/SectionContext.js
@@ -4,17 +4,15 @@ import { data } from '../../config/robot-facts';
 export default function SectionContext() {
   return (
     <div className="section-context">
-      <p className="section-context__subheading">
-        {data.summary}
-      </p>
+      <p className="section-context__subheading">{data[0].summary}</p>
       <ul className="section-context__list">
         <li className="section-context__list-item">
           <img
             className="section-context__list-img"
             alt="checkmark"
-            src={data.icon}
+            src="../../images/products/product_context-checkmark.svg"
           />
-          <span className="section-context__list-text">{data.pointOne}</span>
+          <span className="section-context__list-text">{data[0].pointOne}</span>
         </li>
         <li className="section-context__list-item">
           <img
