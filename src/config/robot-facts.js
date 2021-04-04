8b095637b398b13ec237117cc7c9985d99d7f04d
Kevin Loughead <kvnloughead@gmail.com>
2021-04-04 00:18:04 -0400

    docs: update config file with comment

--- src/config/robot-facts.js
+++ src/config/robot-facts.js
@@ -4,6 +4,7 @@ export const title = 'Wide choice of transport robots';
 
 export const summary = 'Few words about robots. Lorem ipsum, abra-kadabra,bla-bla-bla, information about robots here.';
 
-export const bulletPoints = [['Some facts here and below'], ['Robots are nice'], ['Flexible'], ['Customizable']];
+// currently up to five bullet points are supported
+export const bulletPoints = [['Some facts here and below'], ['Flexible'], ['Customizable']];
 
 addUniqueId(bulletPoints);
