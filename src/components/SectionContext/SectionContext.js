41130aa7c24d70047933212c7741f3f1618fbc71
Kevin Loughead <kvnloughead@gmail.com>
2021-04-04 11:32:48 -0400

    fix: remove hardcoded checkmark src from SectionContext

--- src/components/SectionContext/SectionContext.js
+++ src/components/SectionContext/SectionContext.js
@@ -1,5 +1,5 @@
 import './SectionContext.css';
-import { summary, bulletPoints } from '../../config/robot-facts';
+import { summary, bulletPoints, checkmark } from '../../config/robot-facts';
 
 export default function SectionContext() {
   return (
@@ -8,11 +8,7 @@ export default function SectionContext() {
       <ul className="section-context__list">
         {bulletPoints.map((point) => (
           <li key={point.id} className="section-context__list-item">
-            <img
-              className="section-context__list-img"
-              alt="checkmark"
-              src="../../images/products/product_context-checkmark.svg"
-            />
+            <img className="section-context__list-img" alt="checkmark" src={checkmark} />
             <span className="section-context__list-text">{point}</span>
           </li>
         ))}
--- src/config/robot-facts.js
+++ src/config/robot-facts.js
@@ -1,7 +1,7 @@
 import { addUniqueId } from '../utils/helpers';
 
 export const title = 'Wide choice of transport robots';
-
+export const checkmark = './images/products/product_context-checkmark.svg';
 export const summary = 'Few words about robots. Lorem ipsum, abra-kadabra,bla-bla-bla, information about robots here.';
 
 // currently up to five bullet points are supported
